9bb5e9581aa0d50561e46a306309f1291c5e3263
eliseevers <2155620@talnet.nl>
2022-03-08 13:23:02 +0100

    opdracht 1 instert into

--- 1-INSERT/taak01 - Insert Into/antwoorden.sql
+++ 1-INSERT/taak01 - Insert Into/antwoorden.sql
@@ -1,1 +1,18 @@
+127.0.0.1/studenten.sql/studenten/		http://localhost/phpmyadmin/index.php?route=/sql&server=1&db=studenten.sql&table=studenten&pos=0
+
+   Weergave van records 0 -  7 (8 totaal, Query duurde 0,0015 seconden.)
+
+
+SELECT * FROM `studenten`
+
+
+student_id	voornaam	achternaam	klas	adres	postcode	geboortedatum	woonplaats	
+1	Elise	Evers	1A	Maalderij 3	1181OP	2005-11-28	Amstelveen	
+2	Marloes	Bakker	9A	2e Kekerstraat	1104EG	1999-12-12	Amsterdam	
+3	Emma	da Silva	9A	Burgemeester Mulstraat 66	1109ET	2001-04-28	Amstelveen	
+4	Mohammed	Atari	9A	Duke Ellingtonstraat 11	1066AK	2002-02-17	Amstelveen	
+5	Estee	Evers	1A	strandvliet 35	1181KM	2004-08-30	Amstelveen	
+6	Hetty	Vrede	1A	strandvliet 105	1181JK	1995-08-04	Amstelveen	
+7	Levi	Appelboom	1A	Bolenstein 755	1173RM	2000-01-22	Amsterdam	
+8	Claire	de Jong	1A	tweede jacob van campen straat 2	1045LM	2000-06-20	Amsterdam	
 
