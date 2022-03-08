21b0f4c5eedb29f3070220dcf7bf22c752d7456c
eliseevers <2155620@talnet.nl>
2022-03-08 13:46:07 +0100

    opdracht 2 instert into

--- 1-INSERT/taak02 - Insert Into 2/antwoorden.sql
+++ 1-INSERT/taak02 - Insert Into 2/antwoorden.sql
@@ -1,1 +1,48 @@
+127.0.0.1/studenten.sql/studenten/		http://localhost/phpmyadmin/index.php?route=/sql&server=1&db=studenten.sql&table=studenten&pos=0
+
+   Weergave van records 0 - 13 (14 totaal, Query duurde 0,0015 seconden.)
+
+
+SELECT * FROM `studenten`
+
+
+student_id	voornaam	achternaam	klas	adres	postcode	geboortedatum	woonplaats	
+1	Elise	Evers	1A	Maalderij 3	1181OP	2005-11-28	Amstelveen	
+2	Marloes	Bakker	9A	2e Kekerstraat	1104EG	1999-12-12	Amsterdam	
+3	Emma	da Silva	9A	Burgemeester Mulstraat 66	1109ET	2001-04-28	Amstelveen	
+4	Mohammed	Atari	9A	Duke Ellingtonstraat 11	1066AK	2002-02-17	Amstelveen	
+5	Estee	Evers	1A	strandvliet 35	1181KM	2004-08-30	Amstelveen	
+6	Hetty	Vrede	1A	strandvliet 105	1181JK	1995-08-04	Amstelveen	
+7	Levi	Appelboom	1A	Bolenstein 755	1173RM	2000-01-22	Amsterdam	
+8	Claire	de Jong	1A	tweede jacob van campen straat 2	1045LM	2000-06-20	Amsterdam	
+11	Tom	Jansen	1A	paarsdreef 40	1234AB	2003-05-23	Amsterdam	
+12	Tim	Hansen	1A	kaarsdreef 47	5678CD	2001-12-13	Amsterdam	
+13	Jip	Pieters	1A	Honsdreef 3	9101EG	1980-12-23	Amsterdam	
+14	Piet	Pietje	
+    NULL
+	
+    NULL
+	
+    NULL
+	
+    NULL
+	amsterdam	
+15	Jonah	Kampen	
+    NULL
+	
+    NULL
+	
+    NULL
+	
+    NULL
+	amsterdam	
+16	Monique	de Zonne	
+    NULL
+	
+    NULL
+	
+    NULL
+	
+    NULL
+	amsterdam	
 
